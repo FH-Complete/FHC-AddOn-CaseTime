997540d6143e62dede86f4c0986dec2dd2f8b568
Gerald Raab <raab@technikum-wien.at>
2015-07-13 10:27:24 +0200

    Urlaubskonto verbergen wenn anspruch = 0

--- cis/init.js.php
+++ cis/init.js.php
@@ -74,7 +74,7 @@ function AddonCaseTimeShowUrlaub(uid)
 			var resturlaub = result.Resturlaub;
 			var aktuellerstand = result.AktuellerStand;
 
-			if(urlaubsanspruch!=undefined)
+			if(urlaubsanspruch!=undefined && urlaubsanspruch!="0")
 			{
 				var content = AddonCaseTimeFormatUrlaub(urlaubsanspruch, resturlaub, aktuellerstand);
 				$('#resturlaub').html(content);
