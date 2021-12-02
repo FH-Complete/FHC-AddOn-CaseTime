fff677acbfab7bd6fe840c41f702a172523e8a4e
Andreas Österreicher <oesi@technikum-wien.at>
2021-12-02 17:19:28 +0100

    Fixed Permissions for CLI, Corrected Config and Path to CIS

--- cronjobs/send_timesheets_vorzeitigAbgeschickt.php
+++ cronjobs/send_timesheets_vorzeitigAbgeschickt.php
@@ -14,7 +14,7 @@
  */
 // ------------------------------------------------------------------------
 
-require_once('../../../config/cis.config.inc.php');
+require_once('../../../config/vilesci.config.inc.php');
 require_once('../config.inc.php');
 require_once('../include/timesheet.class.php');
 require_once('../../../include/basis_db.class.php');
@@ -29,12 +29,7 @@ require_once('../../../include/mitarbeiter.class.php');
 require_once('../../../include/sancho.inc.php');
 require_once('../include/functions.inc.php');
 
-
-$uid = get_uid();
 $db = new basis_db();
-$sprache_obj = new sprache();
-$sprache = getSprache();	// users language
-$sprache_index = $sprache_obj->getIndexFromSprache($sprache);	// users language index (for globals.inc.php)
 
 $date_last_month = new DateTime('first day of last month midnight');
 
@@ -47,15 +42,19 @@ $uid_timesheetsError_arr = array();
 // Wenn das Script ueber die Kommandozeile aufgerufen wird, erfolgt keine Authentifizierung
 if (php_sapi_name() != 'cli')
 {
+	$uid = get_uid();
+
 	$rechte = new benutzerberechtigung();
 	$rechte->getBerechtigungen($uid);
-	
+
 	if(!$rechte->isBerechtigt('admin'))
 	{
 		exit($rechte->errormsg);
 	}
 }
 
+$sprache_obj = new sprache();
+$sprache_index = $sprache_obj->getIndexFromSprache(DEFAULT_LANGUAGE);
 
 // Get all timesheets of last month
 // WHERE 'vorzeitig_abgeschickt' = TRUE
@@ -69,33 +68,33 @@ foreach ($timesheets_vorzeitigAbgeschickt_arr as $timesheet_vorzeitigAbgeschickt
 	$benutzer = new Benutzer($timesheet_vorzeitigAbgeschickt->uid);
 	$full_name = $benutzer->getFullName();	// string full name of user
 	$first_name = $benutzer->vorname;
-	
+
 	// Check for blocking casetime errors
 	$hasCaseTimeError = $timesheet->hasCaseTimeError(
 		$timesheet_vorzeitigAbgeschickt->uid,
 		$date_last_month->format('m'),
 		$date_last_month->format('Y'));
-	
+
 	// Check for missing Bestaetigungen
 	$hasMissingBestaetigung = $timesheet->hasMissingBestaetigung(
 		$timesheet_vorzeitigAbgeschickt->uid,
 		$timesheet_vorzeitigAbgeschickt->timesheet_id
 	);
-	
+
 	// Check if Casetime inserts / updated were made today
 	$timesheet = new Timesheet();
 	$hasCaseTimeChanges_today = $timesheet->hasNewOrChangedTimesToday(
 		$timesheet_vorzeitigAbgeschickt->uid,
 		$date_last_month
 	);
-	
+
 	// Check if times were deleted today
 	$timesheet = new Timesheet();
 	$isSyncedWithCaseTime_today = $timesheet->hasDeletedTimes(
 		$timesheet_vorzeitigAbgeschickt->uid,
 		$date_last_month
 	);
-	
+
 	// If no casetime error
 	// and no Bestaetigung is missing
 	// and no Casetime Inserts or Changes were made today
@@ -108,12 +107,12 @@ foreach ($timesheets_vorzeitigAbgeschickt_arr as $timesheet_vorzeitigAbgeschickt
 		// Get Vorgesetzte
 		$mitarbeiter = new Mitarbeiter();
 		$vorgesetzte_uid_arr = array();	// array with uid of one or more supervisors
-		
+
 		if ($mitarbeiter->getVorgesetzte($timesheet_vorzeitigAbgeschickt->uid))
 		{
 			$vorgesetzte_uid_arr = $mitarbeiter->vorgesetzte;
 		}
-		
+
 		// Send Mail to Vorgesetzte
 		foreach ($vorgesetzte_uid_arr as $vorgesetzten_uid)
 		{
@@ -121,11 +120,11 @@ foreach ($timesheets_vorzeitigAbgeschickt_arr as $timesheet_vorzeitigAbgeschickt
 			$benutzer = new Benutzer($vorgesetzten_uid);
 			$vorgesetzter_vorname = $benutzer->vorname;
 			$to = $vorgesetzten_uid. '@'. DOMAIN;
-			
+
 			$subject =
 				'Monatsliste '. $monatsname[$sprache_index][$date_last_month->format('m') - 1]. ' '.
 				$date_last_month->format('Y'). ' von '. $full_name;
-			
+
 			// Set mail template fields
 			$fields = array(
 				'firstName' => $vorgesetzter_vorname,
@@ -133,7 +132,7 @@ foreach ($timesheets_vorzeitigAbgeschickt_arr as $timesheet_vorzeitigAbgeschickt
 				'date_monthlist' => $monatsname[$sprache_index][$date_last_month->format('m') - 1]. " ". $date_last_month->format('Y'),
 				'link' => APP_ROOT. "addons/casetime/cis/timesheet.php?timesheet_id=". $timesheet_vorzeitigAbgeschickt->timesheet_id
 			);
-			
+
 			// Send Sancho mail to Vorgesetzte
 			if (sendSanchoMail('Sancho_Content_confirmTimesheet', $fields, $to, $subject, $header_img))
 			{
@@ -141,10 +140,10 @@ foreach ($timesheets_vorzeitigAbgeschickt_arr as $timesheet_vorzeitigAbgeschickt
 				$timesheet = new Timesheet();
 				$timesheet->timesheet_id = $timesheet_vorzeitigAbgeschickt->timesheet_id;
 				$timesheet->abgeschicktamum = $send_date->format('Y-m-d H:i:s');
-				
+
 				// Save abgeschicktamum
 				$timesheet->save(true);
-			
+
 				$cnt_timesheetsVersendet++;
 			}
 			else
@@ -160,24 +159,24 @@ foreach ($timesheets_vorzeitigAbgeschickt_arr as $timesheet_vorzeitigAbgeschickt
 		// Reset vorzeitig_abgeschickt to FALSE
 		$timesheet = new Timesheet();
 		$timesheet->resetVorzeitigAbgeschickt($timesheet_vorzeitigAbgeschickt->timesheet_id);
-		
+
 		// Send mail to Mitarbeiter and cc to HR
 		$to = $timesheet_vorzeitigAbgeschickt->uid. '@'. DOMAIN;
 		$cc = defined('CASETIME_TIMESHEET_HR_EMAIL') && !empty(CASETIME_TIMESHEET_HR_EMAIL)
 			? CASETIME_TIMESHEET_HR_EMAIL
 			: '';
-		
+
 		$subject = 'Monatsliste '. $monatsname[$sprache_index][$date_last_month->format('m') - 1]. ' '.
 			$date_last_month->format('Y'). ' konnte nicht abgeschickt werden';
-		
+
 		// Set mail template fields
 		$fields = array(
 			'firstName' => $first_name,
 			'date_monthlist' => $monatsname[$sprache_index][$date_last_month->format('m') - 1]. " ". $date_last_month->format('Y'),
-			'link_monthlist' => APP_ROOT. "addons/casetime/cis/timesheet.php?timesheet_id=". $timesheet_vorzeitigAbgeschickt->timesheet_id,
-			'link_zeitaufzeichnung' => APP_ROOT. "cis/private/tools/zeitaufzeichnung.php"
+			'link_monthlist' => CIS_ROOT. "addons/casetime/cis/timesheet.php?timesheet_id=". $timesheet_vorzeitigAbgeschickt->timesheet_id,
+			'link_zeitaufzeichnung' => CIS_ROOT. "cis/private/tools/zeitaufzeichnung.php"
 		);
-		
+
 		// Send Sancho mail to Mitarbeiter and cc to HR informing the timesheet needs to be checked
 		if (sendSanchoMail('TimesheetNichtAbgeschickt',
 			$fields,
@@ -210,6 +209,3 @@ if ($cnt_timesheetsError > 0)
 	echo $nl. "Anzahl Monatslisten fehlerhaft bei folgenden UIDs: ";
 	echo "<br>".'<pre>'. print_r($uid_timesheetsError_arr, -1). '</pre>';
 }
-
-
-
